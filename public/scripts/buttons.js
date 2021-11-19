58e06f13049131571715acce607f3f29ae4c76c8
Alex-Reyne <creativereyne@gmail.com>
2021-11-18 18:39:36 -0600

    add comments to buttons.js, composer.js, and index.html

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,31 +1,50 @@
 $(document).ready(function() {
+  // hides the new-tweet box when page loads
   $(".new-tweet").hide();
+  // hides the to-top button when page loads
   $('#to-top').hide();
 
-
+  // handles the nav button "write a new tweet"
   $('.nav-text').on('click', function() {
 
+    // slides the new-tweet box into view or out of view
+    // when nav button is clicked
     $('.new-tweet').is(':hidden') ? 
     $(".new-tweet").slideDown() :
     $(".new-tweet").slideUp();
 
+    // focus the tweet-text box when the nav button is
+    // clicked
     $("#tweet-text").focus()
 
   });
 
+  // handles the to-top button
   $('#to-top').on('click', function() {
+
+    // scroll to top when clicked
+    // then reveal the new-tweet box
+    // then focus the tweet-text box
     $("html, body").animate({ scrollTop: 0 }, "slow");
     $(".new-tweet").slideDown()
     $("#tweet-text").focus()
+
   });
 
+  // reaveal/hide the to-top button
   $(document).scroll(function() {
+    // counts how many pixels you are from
+    // the top of the page
     const y = $(this).scrollTop();
+
+    // reveals/hides the button if you are 100 pixels
+    //from the top of the page.
     if (y > 100) {
       $('#to-top').slideDown();
     } else {
       $('#to-top').slideUp();
     }
+
   });
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
 
+  // counts the characters you've types in the new tweet box.
   $('#tweet-text').on("input", function(event) {
-    const length = $(this).val().length
-    const max = 140;
+    const length = $(this).val().length // length of what you've typed so far
+    const max = 140; // Max chars allowed
     const charLeft = max - length;
-    const counter = 
+    const counter = // finds the div .counter
     $(this).parentsUntil(".new-tweet")
       .find(".counter")
 
+    // changes the counter text when you type.
     counter.text(charLeft);
 
+    // adds an ID to the counter that will change the color to red
+    // if you've gone over the max characters (140)
     if (charLeft < 0) {
       $(counter).attr("id", "negative"); 
     } else {
